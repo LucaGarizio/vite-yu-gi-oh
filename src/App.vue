29fbd9c63f8fa16535468e1f26477c1e210572d5
Luca Garizio <garizioluca2@gmail.com>
2024-01-08 14:54:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default{
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+    <AppHeader />
+</template>
+
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/general.scss' as *; 
+
+</style>
